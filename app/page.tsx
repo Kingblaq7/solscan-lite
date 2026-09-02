4aebec8bb22fa09137f06e15638deb3460b19853
Kingblaq7 <166668343+Kingblaq7@users.noreply.github.com>
2026-09-02 03:46:42 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,28 @@
 
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import SearchCard from "@/components/SearchCard";
+import ResultCard from "@/components/ResultCard";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+
+      <Navbar />
+
+      <section className="container fade-up">
+
+        <Hero />
+
+        <SearchCard />
+
+        <ResultCard />
+
+      </section>
+
+      <Footer />
+
+    </main>
+  );
+}
