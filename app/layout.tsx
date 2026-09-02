1d03e0fbf7c2b4ae531083033cbf177c04cc379d
Kingblaq7 <166668343+Kingblaq7@users.noreply.github.com>
2026-09-02 03:46:16 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "SolScan Lite",
+  description: "Premium Solana Wallet Inspector",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
